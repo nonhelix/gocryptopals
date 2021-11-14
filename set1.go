842c6fe18743b92ff6e6e33f1e22c7260c66478b
Zhao Meng <nonhelix@Zhaos-MacBook-Pro.local>
2021-11-14 16:48:06 +0800

    set 6

--- set1.go
+++ set1.go
@@ -4,6 +4,8 @@ import (
 	"encoding/base64"
 	"encoding/hex"
 	"log"
+	"math"
+	"math/bits"
 	"unicode/utf8"
 )
 
@@ -54,14 +56,47 @@ func singleXOR(in []byte, key byte) []byte {
 }
 func findSingleXORKey(in []byte, c map[rune]float64) ([]byte, float64) {
 	var res []byte
-	var lastScore float64
+	var bestScore float64
 	for key := 0; key < 256; key++ {
 		out := singleXOR(in, byte(key))
 		score := scoreEnglish(string(out), c)
-		if score > lastScore {
+		if score > bestScore {
 			res = out
-			lastScore = score
+			bestScore = score
 		}
 	}
-	return res, lastScore
+	return res, bestScore
+}
+
+func repeatingXOR(in, key []byte) []byte {
+	res := make([]byte, len(in))
+	for i := range in {
+		res[i] = in[i] ^ key[i%len(key)]
+	}
+	return res
+}
+
+func hammingDistance(a, b []byte) int {
+	if len(a) != len(b) {
+		panic("different length for the two")
+	}
+	var res int
+	for i := range a {
+		res += bits.OnesCount8(a[i] ^ b[i])
+	}
+	return res
+}
+
+func findRepeatXORSize(in []byte) int {
+	var res int
+	var bestScore float64 = math.MaxFloat64
+	for keyLen := 2; keyLen < 40; keyLen++ {
+		a, b := in[:keyLen*5], in[keyLen*5:keyLen*10]
+		score := float64(hammingDistance(a, b)) / float64(keyLen)
+		if score < bestScore {
+			res = keyLen
+			bestScore = score
+		}
+	}
+	return res
 }
--- set1_test.go
+++ set1_test.go
@@ -2,6 +2,7 @@ package cryptopals
 
 import (
 	"bytes"
+	"encoding/base64"
 	"encoding/hex"
 	"io/ioutil"
 	"strings"
@@ -19,13 +20,13 @@ func TestProblemt1(t *testing.T) {
 	}
 }
 func TestProblem2(t *testing.T) {
-	res := xor(hexDecode(t, "1c0111001f010100061a024b53535009181c"), hexDecode(t, "686974207468652062756c6c277320657965"))
-	if !bytes.Equal(res, hexDecode(t, "746865206b696420646f6e277420706c6179")) {
+	res := xor(decodeHex(t, "1c0111001f010100061a024b53535009181c"), decodeHex(t, "686974207468652062756c6c277320657965"))
+	if !bytes.Equal(res, decodeHex(t, "746865206b696420646f6e277420706c6179")) {
 		t.Error("wrong answer", res)
 	}
 }
 
-func hexDecode(t *testing.T, s string) []byte {
+func decodeHex(t *testing.T, s string) []byte {
 	v, err := hex.DecodeString(s)
 	if err != nil {
 		t.Fatal("failed to decode hex", s)
@@ -47,23 +48,52 @@ func TestProblem3(t *testing.T) {
 	for char, val := range corpus {
 		t.Logf("%c:%.5f", char, val)
 	}
-	res, _ := findSingleXORKey(hexDecode(t, "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"), corpus)
+	res, _ := findSingleXORKey(decodeHex(t, "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"), corpus)
 	t.Logf("%s", string(res))
 }
 func TestProblem4(t *testing.T) {
-	text, err := ioutil.ReadFile("4.txt")
-	if err != nil {
-		t.Fatal(err)
-	}
-	var lastScore float64
+	text := readFile(t, "4.txt")
+	var BestScore float64
 	var res []byte
 	for _, line := range strings.Split(string(text), "\n") {
-		line := hexDecode(t, line)
+		line := decodeHex(t, line)
 		out, score := findSingleXORKey(line, corpus)
-		if score > lastScore {
+		if score > BestScore {
 			res = out
-			lastScore = score
+			BestScore = score
 		}
 	}
 	t.Logf("%s", res)
+}
+func TestProblem5(t *testing.T) {
+	input := []byte(`Burning 'em, if you ain't quick and nimble
+I go crazy when I hear a cymbal`)
+	res := repeatingXOR(input, []byte("ICE"))
+	if !bytes.Equal(res, decodeHex(t, "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f")) {
+		t.Error("wrong result", res)
+	}
+}
+
+func TestProblem6(t *testing.T) {
+	res := hammingDistance([]byte("this is a test"), []byte("wokka wokka!!!"))
+	if res != 37 {
+		t.Fatal("wrong hemming Distance")
+	}
+	text := decodeBase64(t, string(readFile(t, "6.txt")))
+	t.Log("likely size:", findRepeatXORSize(text))
+}
+
+func decodeBase64(t *testing.T, s string) []byte {
+	v, err := base64.StdEncoding.DecodeString(s)
+	if err != nil {
+		t.Fatal("failed to decode base64", s)
+	}
+	return v
+}
+func readFile(t *testing.T, f string) []byte {
+	test, err := ioutil.ReadFile(f)
+	if err != nil {
+		t.Fatal("cant open file")
+	}
+	return test
 }
